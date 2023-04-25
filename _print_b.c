078142fa124fa864bdeec0fda05f2b16ba11b73c
Nusroh123 <oyedelenusroh@gmail.com>
2023-04-24 17:47:02 -0700

    remove conflicting function

--- _print_b.c
+++ _print_b.c
@@ -13,20 +13,35 @@ int pr_binary(va_list args)
 
 	return (print_b(b, sum));
 }
+
 /**
- * print_b - convert int to binary
+ * print_b - convert int to binary or octal
  * @n: number to be converted
- * @sum: 0
+ * @sum: 0.
+ * @base: 2 or 8.
  * Return: number of char printed
  */
-int print_b(unsigned int n, int sum)
+int print_b(unsigned int n, int sum, int base)
 {
 	unsigned int x = n;
 
-	x /= 2;
+	x /= base;
 
 	if (x != 0)
-		sum = print_b(x, sum);
-	sum += print('0' + n % 2);
+		sum = print_b(x, sum, base);
+	sum += print('0' + n % base);
 	return (sum);
+}
+
+/**
+ * print_octal - convert int into octal
+ * @args: numbers to be converted
+ * Return: number of digist printed.
+ */
+int pr_octal(va_list args)
+{
+	int sum = 0;
+	unsigned int o = va_arg(args, unsigned int);
+
+	return (print_b(o, sum, 8));
 }
