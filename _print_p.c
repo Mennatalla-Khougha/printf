8302d2bcd1ebf4bd7952c0110ba25d3d698435dc
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-26 03:39:42 +0200

    fix empty pointer

--- _print_p.c
+++ _print_p.c
@@ -11,6 +11,8 @@ int pr_pointer(va_list args)
 	int sum = 0;
 	unsigned long int p = va_arg(args, unsigned long int);
 
+	if (p == 0)
+		return (pr_string("(nil)"));
 	sum += print('0');
 	sum += print('x');
 	return (sum + print_p(p, 0));
@@ -27,8 +29,6 @@ int print_p(unsigned long int p, int sum)
 {
 	unsigned long int x = p;
 
-	if (x == 0)
-		return (pr_string("(nil)"));
 	x /= 16;
 	if (x != 0)
 		sum = print_p(x, sum);
